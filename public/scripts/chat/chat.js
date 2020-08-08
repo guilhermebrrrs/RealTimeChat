bc4b758ad6f8de7cc8c9cc38c36f447576aba9b9
Guilherme Barra Orsi <guilhermebrrrs@hotmail.com>
2020-08-08 16:37:49 -0300

    chat finished - 95% responsive

--- public/scripts/chat/chat.js
+++ public/scripts/chat/chat.js
@@ -9,24 +9,30 @@ const queryString = location.search.slice(1)
 const queryStringParts = queryString.split('&')
 const userData = {}
 
+queryStringParts.forEach((part) => {
+  const keyValue = part.split('=')
+  const key = keyValue[0]
+  const value = keyValue[1].split('+').join(' ')
+  userData[decodeURIComponent(key)] = decodeURIComponent(value)
+})
+
+userName.innerHTML = userData.username
+roomId.innerHTML = userData.roomid
+
 function renderOwnerMessage(message) {
   const divMessage = document.createElement('div')
 
   divMessage.classList.add('owner-message')
-  divMessage.innerHTML = `<span class="message-user-name">${message.userName}</span><p class="user-message">${message.message}</p><span class="message-time">${message.messageTime}</span>`
+  divMessage.innerHTML = `<span class="message-user-name">${message.userName}</span><p class="user-message">${message.message}</p><span class="message-time">${message.messageHours + ':' + message.messageMinutes}</span>`
   messages.appendChild(divMessage)
-
-  messageBox.value = ''
 }
 
 function renderReceivedMessage(message) {
   const divMessage = document.createElement('div')
 
   divMessage.classList.add('received-message')
-  divMessage.innerHTML = `<span class="message-user-name">${message.userName}</span><p class="user-message">${message.message}</p><span class="message-time">${message.messageTime}</span>`
+  divMessage.innerHTML = `<span class="message-user-name">${message.userName}</span><p class="user-message">${message.message}</p><span class="message-time">${message.messageHours + ':' + message.messageMinutes}</span>`
   messages.appendChild(divMessage)
-
-  messageBox.value = ''
 }
 
 function renderWelcomeMessage(socketId, userName) {
@@ -42,44 +48,50 @@ function renderWelcomeMessage(socketId, userName) {
 
     divMessage.classList.add('welcome-new-user')
     divMessage.innerHTML = `••• <span>${userName}</span> entrou na sala •••`
-    messages.appendChild(divMessage)    
+    messages.appendChild(divMessage)
   }
 }
 
-queryStringParts.forEach((part) => {
-  const keyValue = part.split('=')
-  const key = keyValue[0]
-  const value = keyValue[1].split('+').join(' ')
-  userData[decodeURIComponent(key)] = decodeURIComponent(value)
-})
 
-userName.innerHTML = userData.username
-roomId.innerHTML = userData.roomid
 
 sendButton.addEventListener('click', () => {
-  const messageObject = {
-    userSocket: socket.id,
-    userName: userData.username,
-    roomId: userData.roomid,
-    message: messageBox.value,
-    messageTime: new Date()
-  }
+  if (messageBox.value) {
+    const time = new Date()
 
-  socket.emit('chatMessage', messageObject)
+    const messageObject = {
+      userSocket: socket.id,
+      userName: userData.username,
+      roomId: userData.roomid,
+      message: messageBox.value,
+      messageTime: time,
+      messageHours: time.getHours(),
+      messageMinutes: time.getMinutes()
+    }
+
+    socket.emit('chatMessage', messageObject)
+    messageBox.value = ''
+  }
 })
 
-messageBox.addEventListener('submit', (event) => {
-  event.preventDefault()
+messageBox.addEventListener('keydown', (event) => {
+  if (event.key == 'Enter') {
+    if (messageBox.value) {
+      const time = new Date()
 
-  const messageObject = {
-    userSocket: socket.id,
-    userName: userName.value,
-    roomId: roomId.value,
-    message: messageBox.value,
-    messageTime: new Date()
+      const messageObject = {
+        userSocket: socket.id,
+        userName: userData.username,
+        roomId: userData.roomid,
+        message: messageBox.value,
+        messageTime: time,
+        messageHours: time.getHours(),
+        messageMinutes: time.getMinutes()
+      }
+
+      socket.emit('chatMessage', messageObject)
+      messageBox.value = ''
+    }
   }
-
-  socket.emit('chatMessage', messageObject)
 })
 
 socket.emit('joinRoom', userData)
@@ -91,18 +103,16 @@ socket.on('welcomeMessage', (user) => {
 socket.on('message', (messageObject) => {
   if (messageObject.userSocket === socket.id) {
     renderOwnerMessage(messageObject)
-    console.log('owner', messageObject)
   }
   if (messageObject.userSocket !== socket.id) {
     renderReceivedMessage(messageObject)
-    console.log('received', messageObject)
   }
 })
 
 socket.on('userDisconnects', (user) => {
   const divMessage = document.createElement('div')
 
-    divMessage.classList.add('user-disconnects')
-    divMessage.innerHTML = `••• ${user.username} saiu da sala •••</span>`
-    messages.appendChild(divMessage)
+  divMessage.classList.add('user-disconnects')
+  divMessage.innerHTML = `••• ${user.username} saiu da sala •••</span>`
+  messages.appendChild(divMessage)
 })
